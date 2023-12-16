4346d5b971228efaea216c6004d85713762d0c04
Adithyababu <adithyababu865@gmail.com>
2023-12-16 07:47:17 +0530

    project code

--- src/App.js
+++ src/App.js
@@ -1,23 +1,32 @@
-import logo from './logo.svg';
 import './App.css';
-
+import {Parallax} from 'react-parallax'
+import parallax1 from './images/parallax1.jpg'
+import parallax2 from './images/parallax2.jpg'
+import parallax3 from './images/parallax3.jpg'
+import parallax4 from './images/parallax4.jpg'
 function App() {
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <Parallax strength={300} bgImage={parallax1}>
+        <div className='content'>
+          <div className='text'>Lorem Ipsum is simply dummy text of the printing and typesetting industry. Lorem Ipsum has been the industry's standard dummy text ever since the 1500s, when an unknown printer took a galley of type and scrambled it to make a type specimen book. </div>
+        </div>
+      </Parallax>
+      <Parallax strength={600} bgImage={parallax2}>
+        <div className='content'>
+          <div className='text'>Lorem Ipsum is simply dummy text of the printing and typesetting industry. Lorem Ipsum has been the industry's standard dummy text ever since the 1500s, when an unknown printer took a galley of type and scrambled it to make a type specimen book. </div>
+        </div>
+      </Parallax>
+      <Parallax strength={600} bgImage={parallax3}>
+        <div className='content'>
+          <div className='text'>Lorem Ipsum is simply dummy text of the printing and typesetting industry. Lorem Ipsum has been the industry's standard dummy text ever since the 1500s, when an unknown printer took a galley of type and scrambled it to make a type specimen book. </div>
+        </div>
+      </Parallax>
+      <Parallax strength={300} bgImage={parallax4}>
+        <div className='content'>
+          <div className='text'>Lorem Ipsum is simply dummy text of the printing and typesetting industry. Lorem Ipsum has been the industry's standard dummy text ever since the 1500s, when an unknown printer took a galley of type and scrambled it to make a type specimen book. </div>
+        </div>
+      </Parallax>
     </div>
   );
 }
